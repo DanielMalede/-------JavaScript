38f0c9846af9d22c767d7cab74d99d48fd605297
DanielMalede <danielmalede@gmail.com>
2022-05-16 18:36:22 +0300

    changes

---
+++ main.js
@@ -0,0 +1,605 @@
+// function printisrael() {
+//    console.log("ISRAEL"); 
+// }
+// printisrael()
+
+// function printDANIEL() {
+// console.log("DANIEL");    
+// }
+// printDANIEL()
+
+
+// function number() {
+//     console.log(50);
+    
+// }
+// number()
+
+// function number() {
+// var input = 50
+// for (var i = 0;i<5;i++){
+//     console.log(50);
+// }
+// }
+// number()
+
+// function sumavg (){
+// var num1 = 1
+// var num2 = 2
+// var num3 = 3
+// var num4 = 4
+// var sum = num1+num2+num3+num4
+// var avg = sum/4
+// console.log(sum,avg);
+// }
+// sumavg()
+
+// function name(){
+// var height = 6
+// var width = 10
+// console.log(height*width);
+// }
+// name()
+
+// function helper(){
+// var num1 = 1
+// var num2 = 2
+// console.log(num1,num2);
+// var helper = num2
+// num2 = num1
+// num1 = helper
+// console.log(num1,num2);
+// }
+// helper()
+
+// function avgsum(){
+// var num1 = +prompt()
+// var num2 = +prompt()
+// var num3 = +prompt()
+// var num4 = +prompt()
+// var sum = num1+num2+num3+num4
+// var avg = sum/4
+// console.log(avg);
+// }
+// avgsum()
+
+// function widthheight(){
+// var height = +prompt()
+// var width = +prompt()
+// console.log(height*width);
+// }
+// widthheight()
+
+// function helper(){
+// var num1 = +prompt()
+// var num2 = +prompt()
+// console.log(num1,num2);
+// var helper = num1
+// num1 = num2
+// num2 = helper
+// console.log(num1,num2);
+// }
+// helper()
+
+// function num1num2() {
+// var num1 = +prompt() +prompt()
+// var num2 = +prompt() +prompt()
+// console.log(num1,num2);    
+// }
+// num1num2()
+
+// function number() {
+// var num1 = +prompt()
+// var num2 = +prompt()
+// console.log(num1,num2);   
+// }
+// number()
+
+// function a1a2 (){
+// var a1 = "a"
+// var a2 = "1"
+// console.log(a1,a2);
+// }
+// a1a2()
+
+// function a() {
+// var a = true
+// console.log(a);  
+// }
+// a()
+
+// function ab() {
+// console.log(a=true,b=true); 
+// console.log(a=false,b=false); 
+// console.log(a=true,b=false); 
+// console.log(a=false,b=true); 
+// }
+// ab()
+
+// function printmul() {
+// var num1 = +prompt()
+// var num2 = +prompt()
+// var num3 = +prompt()
+// var mul = num1*num2*num3
+// console.log(mul);
+// }
+// printmul()
+
+// function modolu(params) {
+// var first = +prompt()
+// var second = +prompt()
+// var result = first%second
+// console.log(result);
+// }
+// modolu()
+
+// function plus() {
+// var input = +prompt()
+// console.log(++input);   
+// }
+// plus()
+
+// function dubele() {
+// var input = +prompt()
+// var input = input*3
+// console.log(input);
+// }
+// dubele()
+
+// function printradius(params) {
+// var radius = +prompt()
+// var scope = 2*3.14*radius
+// var area = 3.14**radius
+// console.log(scope,area); 
+// }
+// printradius()
+
+
+// function nameavg() {
+// var player1 = prompt("enter your name")
+// var player2 = prompt("enter your name")
+// var player3 = prompt("enter your name")
+// var player1age = +prompt("enter your age")
+// var player2age = +prompt("enter your age")
+// var player3age = +prompt("enter your age")
+// var sum = player1age+player2age+player3age
+// var avg = sum/3
+// console.log(player1,player2,player3,avg);    
+// }
+// nameavg()
+
+// function double() {
+// var input = +prompt("enter number")
+// console.log(input*3); 
+// }
+// double()
+
+///////////////////////////////////////////////////////////////////////////
+                                    // Precedence 
+///////////////////////////////////////////////////////////////////////////    
+// function plusone(params) {
+// b = 4
+// d = 4 + b+1
+// console.log(b,d);  
+// }
+// plusone()
+
+// function plusone(params) {
+// b = 4
+// d = 4 + b-1
+// console.log(b,d);  
+// }
+// plusone()
+
+//////////////////////////////////////////////////////////////////////////
+                                        //  תנאים
+//////////////////////////////////////////////////////////////////////////                                     
+// function printgreaterthen10(){
+// var num = +prompt("enter number")
+// if (num>10){
+//     console.log("greater then 10");
+// }
+// }
+// printgreaterthen10()
+
+// function printgreaterthen10(){
+// var num = +prompt("enter number")
+// if (num>10){
+//     console.log("greater then 10");
+// }
+// else{
+//     console.log("less then 10");
+// }
+// }
+// printgreaterthen10()
+
+// function printgreaterthen10(){
+//     var num = +prompt("enter number")
+// if (num>10){
+//     console.log("greater then 10");
+// }
+// else if(num<10){
+//     console.log("less then 10");
+// }
+// else if(num==10){
+//     console.log("equal to 10");
+// }
+// }
+// printgreaterthen10()
+
+// function ten() {
+// var first = +prompt()
+// var second = +prompt()
+// if(first > second){
+//     console.log(first);
+// }
+// else if (first==second){
+//     console.log("values are equal");
+// }
+// else{
+//     console.log(second);
+// }
+// }
+// ten()
+
+/////////////////////////////////////////////////////////////////////////
+                                    //   לולאות
+//////////////////////////////////////////////////////////////////////////                                    
+// function number() {
+// var i = 1
+// while (i<=100) {
+//     console.log(i);
+//     i++
+// }  
+// }
+// number()
+
+// function sumavg(){
+// var num = 0 
+// var sum = 0 
+// while (num<5) {
+//     var input = +prompt()
+//     sum += input
+//     var avg = sum/5        
+//     num++
+// }
+// console.log(sum,avg);
+// }
+// sumavg()
+
+// function number() {
+// var num = 2
+// while (num<=50) {
+//     if (num%2==0) {
+//         console.log(num);
+//     }
+//     num++   
+// }
+// }
+// number()
+
+// function number() {
+// var num = 1
+// while (num<=50) {
+//     if (num%3==1) {
+//         console.log(num);
+//     }
+//     num++   
+// }
+// }
+// number()
+
+// var input = +prompt()
+// var i = 0
+// while (i>=input) {
+//     var sum =  0
+//     sum = input*input
+//     i++
+// }
+// console.log(sum);
+
+/////////////////////////////////////////////////////
+                                // פונקציות
+/////////////////////////////////////////////
+// function printhello() {
+//     var i = 0
+//     while (i<3) {
+//     document.write("hello world")
+//     i++
+//     }
+// }
+// printhello()
+
+// function highnum() {
+//     var num1 = +prompt()
+//     var num2 = +prompt()
+//     if (num1>num2){
+//         return console.log(num1); 
+//     }
+//     else{
+//         return console.log(num2); 
+//     }   
+// }
+// highnum()
+
+// function summ() {
+//     var num1 =+prompt()
+//     var num2 =+prompt()
+//     var sum = num1+num2
+//     return console.log(sum);
+// }
+// summ()
+
+// function summ(num) {
+// var num = +prompt()
+// var sum = 0
+// for(var i = 0;num>i;num--)
+//     sum+= num
+//     return console.log(sum);
+// }
+// summ()
+
+// function mixswitch(num1,num2) {
+//     var helper = num1
+//     num1 = num2
+//     num2 = helper
+//     console.log(num1,num2);
+// } 
+// mixswitch(10,6)
+
+// function printhello() {
+//     document.write("hello")
+// }
+// printhello()
+// printhello()
+// printhello()
+// printhello()
+// printhello()
+
+// function minimum(num1,num2) {
+//     if(num1<=num2){
+//         return num1
+//     }
+//     if(num1>num2){
+//         return num2
+//     }
+// }
+// console.log(minimum(10,5)); 
+
+// function minimum(num1,num2,num3) {
+//     if(num1<num2){
+//         return console.log(num1);
+//     }
+//     if(num1<num3){
+//         return console.log(num1);
+//     }
+//     if(num2<num3){
+//         return console.log(num2);
+//     }
+//     if(num2<num1){
+//         return console.log(num2);
+//     }    
+//     if(num3<num2){
+//         return console.log(num3);
+//     }
+//     if(num3<num1){
+//         return console.log(num3);
+//     }
+//     if(num1==num2==num3){
+//         return console.log(num1);
+//     }    
+// }
+// minimum(1,1,1)
+
+// function avrage(num1,num2,num3){
+//     var sum = num1+num2+num3
+//     var avg = sum/3
+//     console.log(avg);
+// }
+// avrage(5,5,5)
+
+// function summ(num1,num2,num3,num4){
+//     var sum = num1+num2+num3+num4
+//     console.log(sum);  
+// }
+// summ(55,2,2,2)
+
+// function component(base,exponent) {
+//     return base**exponent;
+// }
+// console.log(component(2,4)); 
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////מערכים!!//////////////////////////////////////////////////////
+
+// var array = [5,3,7,8,11]
+// var sum = 0
+// for (var i = 0; i<array.length; i++) {
+//     sum += array[i] 
+
+// }
+// console.log(sum,sum/array.length);
+
+// var name = ["daniel", "batel", "yosi", "eldad", "jonatan"]
+// console.log(name);
+
+// function numbers(number,array) {
+//     for (var i = 0; i < array.length; i++) {
+//         if (number == array[i]) {
+//             return true;
+//         }
+//     }
+//     return false; 
+// }
+// numbers(2,[2,3,1,1,2])
+
+// function numbers(num=[]) {
+//     for (var i = 0; i < num.length; i++) {        
+//     if(num[i]%2==0){
+//          console.log(num[i]);
+//     }
+//     }
+// }
+// numbers([1,2,3,4,5,6,8,10])
+
+// function copyNumbers(num=[],num1=[]) {
+//     if (num.length==num1.length){
+//         return console.log(true); 
+//     }  
+//     return console.log(false);   
+// }
+// copyNumbers([1,2,3],[1,2,2,3])
+
+// function maxNum(array) {
+//     var max = array[0]
+//     for (var i = 0; i < array.length; i++) {
+//         if (max<=array[i]) {
+//             max= array[i]
+//         }                
+//     }
+// //     return max    
+// // }
+// // console.log(maxNum([1,2,3,4])); 
+
+// function min(Array) {
+//     var minnum = Array[0]
+//     var index = Array[0]
+//     for (var i = 0; i < Array.length;i++) {
+//     if (Array[i]<=minnum) {
+//         minnum = Array[i]
+//         index = [i]
+//     }        
+//     }
+//     return console.log(i); 
+// }
+// min([2,4,8,6,2,2,3]); 
+
+// function sum(array) {
+//     var sumnum = 0
+//     for (var i = 0; i < array.length; i++) {
+//             sumnum = array[i] + sumnum
+        
+//     }
+//     return console.log(sumnum); 
+    
+// }
+// sum([10,10,2,1]);
+
+// function dubele(array) {
+//     var min = array[0]
+//     var max = array[0]
+//     for (var i = 0; i < array.length; i++) {
+//     if(array[i]<=min){
+//         min = array[i]
+//      } 
+//     else if (array[i]>=max)  {
+//         max = array[i]
+//     }
+//     }
+//     return console.log(min*max);
+// }
+// dubele([3,1,4,5])
+
+
+// function indexFun(array) {
+//     var max = 0
+//     var index = 0
+//     for (var i = 0; i < array.length; i++){
+//         if(array[i]%2==0){
+//             if (array[i]>max){
+//                 max= array[i]
+//                 index = [i]
+//             }
+//         }
+//     }
+//     console.log(max,index);
+// }
+// indexFun([6,2,3,4,5,5,5,4])
+
+// function numbers(array) {
+//     var max = 0
+//     for (var i = 0; i < array.length; i++) {
+//         if (array[i]%2==1) {
+//             max = array[i] + max
+//         }
+//     }          
+//     return console.log(max);
+
+// }
+// numbers([1,7,2,3,4,5])
+
+// function addName(name,array) {
+//     array.push(name)
+//     console.log(array);
+    
+// }
+// addName("aviel",["dan","dal","chen"])
+
+// function email(singelemail,array) {
+//     array.push(singelemail)
+//     console.log(array);
+// }
+// email("daniel@gmail.com",["asdf@gmail.com","sdfg@gmail.com","bhub@gmail.com"])
+
+// function numbers(number,array) {
+//     if (number%2==0) {
+//         array.push(number)  
+//     }
+//     else{
+//         array.unshift(number)
+//     }     
+//     console.log(array); 
+// }
+// numbers(4,[1,2,3,4,5])
+
+function numbers(array,number) {
+    for (var i = 0; i < array.length; i++) {
+        if (number==array[i]) {
+            array.splice(i,1)
+        }
+        else if (number!=array[i]){
+            array.push(number)
+        }
+    }    
+    console.log (array)
+}
+numbers ([1,2,3,4,5],1); 
+
+// function numbers(number,array) {
+//     var max = 0
+//     for (var i = 0; i < array.length; i++) {
+//         if (array[i]>max) {
+//             max = array[i]
+//         }   
+//     }        
+//     if (number>max){
+//             array.unshift(number)
+//     }
+//     else{
+//             array.push(number)
+//     }
+//     console.log(array);
+// }
+// numbers(1,[1,2,3,4,5])
+
+// function numbers(number,array) {
+//     if (number<array.length){
+//         array.splice(number,1)
+//     }
+//     console.log(array);
+// }
+// numbers(3,[1,2,3,4,5,6])
+
+
+
+
+// _________________________________CHALLENGES!______________________________________________________________________
+
+
+
+
+
+
+
+
